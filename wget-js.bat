c97741e48d2e94cdfa94ce78c9169a6edc6d5e58
sworc <lightbox@nifty.com>
2022-05-07 18:27:02 +0900

    Add files via upload

---
+++ wget-js.bat
@@ -0,0 +1,3 @@
+@echo off
+
+cscript.exe httpget.js https://winofsql.jp/image/daz/2021-08-03-1.jpg wget-js.jpg
---
+++ wget-vbs.bat
@@ -0,0 +1,3 @@
+@echo off
+
+cscript.exe httpget.vbs https://winofsql.jp/image/daz/2021-08-03-1.jpg wget-vbs.jpg
